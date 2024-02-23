1556a29c14bd07bae57a9473c3b5ce0c639af948
Joel Fentes <82660102+JoelFentes@users.noreply.github.com>
2024-02-22 21:11:00 -0300

    commit

--- sorteador/sorteador_equipes.py
+++ sorteador/sorteador_equipes.py
@@ -25,21 +25,16 @@
 
 import random
 
-# Receber nomes dos alunos
 alunos = input("Digite os nomes dos alunos separados por vírgula: ").split(",")
 
-# Receber quantidade de pessoas por grupo
 tamanho_grupo = int(input("Digite a quantidade de pessoas por grupo: "))
 
-# Calcular quantidade total de grupos e quantidades de alunos restantes
 quantidade_alunos = len(alunos)
 quantidade_grupos = quantidade_alunos // tamanho_grupo
 alunos_restantes = quantidade_alunos % tamanho_grupo
 
-# Embaralhar a lista de alunos
 random.shuffle(alunos)
 
-# Dividir os alunos em grupos
 grupos = []
 inicio = 0
 for i in range(quantidade_grupos):
@@ -48,11 +43,9 @@ for i in range(quantidade_grupos):
     grupos.append(grupo)
     inicio = fim
 
-# Adicionar alunos restantes em grupos existentes
 if alunos_restantes > 0:
     for i in range(alunos_restantes):
         grupos[i].append(alunos[quantidade_grupos * tamanho_grupo + i])
 
-# Exibir os grupos
 for i, grupo in enumerate(grupos, start=1):
     print(f"Grupo {i}: {', '.join(grupo)}")
